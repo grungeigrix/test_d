a3edd3c33d448684a463e7487ae56a49700d5df8
grungeigrix <72521952+grungeigrix@users.noreply.github.com>
2021-04-11 20:23:45 +0300

    Add numbers word

--- new_project/views.py
+++ new_project/views.py
@@ -7,5 +7,6 @@ def home(request):
 
 def reverse(request):
 	user_text = request.GET['usertext']
+	l_s = len(user_text.split())
 	reversed_text = user_text[::-1] 
-	return render(request,'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text})
+	return render(request,'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text, 'ls':l_s})
